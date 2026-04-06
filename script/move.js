5a25dda2234983aafd0fb31391d4b8f957e9b960
aglassoffog <aglassoffog@gmail.com>
2026-04-06 22:15:02 +0900

    add move type

--- script/move.js
+++ script/move.js
@@ -1,11 +1,19 @@
-function leftShift(pattern) {
-  pattern.forEach(track => {
-    track.push(track.shift());
+function leftShift(pattern, index) {
+  pattern.forEach((track, trackIndex) => {
+    if(index == null) {
+      track.push(track.shift());
+    } else if (trackIndex === index) {
+      track.push(track.shift());
+    }
   });
 }
 
-function rightShift(pattern) {
-  pattern.forEach(track => {
-    track.unshift(track.pop());
+function rightShift(pattern, index) {
+  pattern.forEach((track, trackIndex) => {
+    if(index == null) {
+      track.unshift(track.pop());
+    } else if (trackIndex === index) {
+      track.unshift(track.pop());
+    }
   });
 }
--- script/moveUI.js
+++ script/moveUI.js
@@ -1,46 +1,35 @@
 function initMoveUI() {
-  // const control = document.getElementById("moveControl");
+  const options = {
+    Left: leftShift,
+    Right: rightShift
+  };
 
-  // const h4t = document.createElement("h4");
-  // h4t.textContent = "All";
-  // control.appendChild(h4t);
-
-  // const moveBtn0 = document.createElement("button");
-  // moveBtn0.classList.add("button");
-  // moveBtn0.textContent = "Left";
-
-  const options = [
-    "Left",
-    "Right"
-  ];
-
-
-
-  const seq1Left = document.getElementById("move-seq1-left");
-  seq1Left.onclick = () => {leftShift(patterns[0]); updateUI(0);}
-  const seq1Right = document.getElementById("move-seq1-right");
-  seq1Right.onclick = () => {rightShift(patterns[0]); updateUI(0);}
-
-  const seq2Left = document.getElementById("move-seq2-left");
-  seq2Left.onclick = () => {leftShift(patterns[1]); updateUI(1);}
-  const seq2Right = document.getElementById("move-seq2-right");
-  seq2Right.onclick = () => {rightShift(patterns[1]); updateUI(1);}
-
-  const allLeft = document.getElementById("move-all-left");
-  allLeft.onclick = () => {
-    leftShift(patterns[0]);
-    leftShift(patterns[1]);
-    updateUI(0);
-    updateUI(1);
+  for(let i=0;i<3;i++){
+    const div = document.getElementById("move-seq"+(i+1))
+    Object.keys(options).forEach(key => {
+      for(let k=0;k<4;k++){
+        if (i === 2 && k > 0) {
+        } else {
+          const button = document.createElement("button");
+          button.className = "button";
+          button.textContent = ((k > 0) ? key.slice(0, 1) + k : key);
+          button.onclick = () => {
+            if (i === 2 && k === 0) {
+              options[key](patterns[0]);
+              options[key](patterns[1]);
+              updateUI(0);
+              updateUI(1);
+            } else {
+              const index = (k > 0) ? k-1 : null;
+              options[key](patterns[i], index);
+              updateUI(i);
+            }
+          }
+          div.appendChild(button);
+        }
+      }
+      const br = document.createElement("br");
+      div.appendChild(br);
+    });
   }
-  const allRight = document.getElementById("move-all-right");
-  allRight.onclick = () => {
-    rightShift(patterns[0]);
-    rightShift(patterns[1]);
-    updateUI(0);
-    updateUI(1);
-  }
-
-
-  // control.appendChild(moveBtn0);
 }
